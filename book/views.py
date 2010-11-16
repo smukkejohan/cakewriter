56e1c5006df37086e264ab8e39b3a94ca1ee3bea
Johan Bichel Lindegaard <mr.bichel@gmail.com>
2010-11-16 18:09:39 +0100

    new stylnew stylee

--- book/views.py
+++ book/views.py
@@ -20,8 +20,17 @@ def chapter(request, chapter_id):
 
 def rating_widget(request, chapter_id):
     chapter = get_object_or_404(Chapter, pk=chapter_id)
+    
+    OPTIONS = (
+        ('1', 'Very poor'),
+        ('2', 'Not that bad'),
+        ('3', 'Average'),
+        ('4', 'Good'),
+        ('5', 'Perfect'),
+    )
+    
     return render_to_response(
-        'book/rating_widget.html', {'chapter': chapter,},
+        'book/rating_widget.html', {'chapter': chapter, 'options': OPTIONS},
         context_instance = RequestContext(request)
     )
     
@@ -58,8 +67,8 @@ def rate_chapter(request, chapter_id, score = None):
             'app_label': 'book',
             'model': 'chapter',
             'object_id': chapter_id,
-            'field_name': 'rating', # this should match the field name defined in your model
-            'score': score, # the score value they're sending
+            'field_name': 'rating',
+            'score': score,
         }
         response = AddRatingFromModel()(request, **params)
         
@@ -71,9 +80,7 @@ def rate_chapter(request, chapter_id, score = None):
             return redirect(chapter)
         
 
-
-##score = chapter.rating.get_rating(request.user, request.META['REMOTE_ADDR'])
-    
+##score = chapter.rating.get_rating(request.user, request.META['REMOTE_ADDR'])  
 #json_serializer = serializers.get_serializer("json")()
 #json_response = json_serializer.serialize(response_dict, ensure_ascii=False, stream=response)
 
