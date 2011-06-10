8d7725c8c77ae5cd75bc9aed67328e8852a203a3
tmpethick <tmpethick@gmail.com>
2011-06-10 16:50:06 +0200

    effect on points when rating chapter

--- static/js/base.js
+++ static/js/base.js
@@ -64,11 +64,21 @@ $(function() {
             	cancelShow: false,
                 captionEl: $caption,
                 callback: function(ui, type, value){
-                    $.post($("#rating-widget").attr("action"), {score: value
-}, function(data){
-                    
+                    $.post($("#rating-widget").attr("action"), {score: value}, 
+                    function(data){
+                        if (!(data=="Vote changed.")){
+                            var oldHTML = document.getElementById('score_int').innerHTML;
+                            var oldNumber = parseInt(oldHTML);
+                            var newHTML = oldNumber+1;
+                            document.getElementById('score_int').innerHTML = newHTML;
+                            $( "#animation" )
+                            .animate({color: "#A80002",fontSize: "30px",textShadow: "#df0003 0px 0px 5px;"}, 500, 
+                            function(){$( "#animation" )
+                            .animate({color:"#df0003", fontSize: "14px",textShadow: "#df0003 0px 0px 0px;"},500);});
+                        }
                     });
                 }
+                
             });
                $caption.appendTo("#rating-widget");
 
