30de033242cfcc8bfa16412405d4ba1ae0e22741
tmpethick <tmpethick@gmail.com>
2011-08-18 14:58:35 +0200

    chapter message javascript added

---
+++ static/js/message.js
@@ -0,0 +1,27 @@
+var show=true;
+var hide=false;
+var close=false;
+$(document).ready(function(){
+
+ $(window).scroll(function () {
+ 
+     if($(window).scrollTop()>=$("#chapter").height()-200 )
+     {
+         if( show==true && close==false)
+         {
+            show=false;
+            hide=true;          
+            $("#message").show("slide", { direction: "right" }, 500);
+         }
+     }else if ($(window).scrollTop()<$("#chapter").height()-200)
+     {
+         if(hide==true && close==false)
+         {
+                show=true;
+                hide=false;                         
+                $("#message").hide("slide", { direction: "right" }, 500);
+         }
+     }
+     
+    });
+ });
